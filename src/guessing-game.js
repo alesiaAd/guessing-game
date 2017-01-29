4e59e7ada77a1921742c0502d00d656d5d131214
alesiaAd <alesia.kurlenia@gmail.com>
2017-01-29 21:41:02 +0300

    Third task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        var middle = (this.min + this.max)/2 + 0.5;
+        middle = Math.floor(middle);
+        this.middle = middle;
+        return middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
